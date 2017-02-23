fbf89642c1841f6634fb5b2ffc5f1b4cba61d5ea
Esphas Kueen <esphas@hotmail.com>
2017-02-23 19:38:51 +0800

    :show_priv (default to self)

--- lib/commands/show_priv.rb
+++ lib/commands/show_priv.rb
@@ -4,6 +4,7 @@ module OpenRobot
       _WRONG_QQ = "森莫鬼啦"
       _NO_PRIV  = "噗噗噗，这谁啊，啥都干不了哦"
       str = str.strip
+      str = OpenRobot.current[3].to_s if str.empty?
       return _WRONG_QQ unless str =~ /^\d{6,13}$/
       privs = Privilege::Entity::execute("select * from privilege").to_h
       res = Privilege.find_user_all_priv(str).map(&privs).join ?,
