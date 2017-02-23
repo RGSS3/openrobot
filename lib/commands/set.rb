fbe9031c10863ed2d8736c9b3ffa3db10cf962d6
Esphas Kueen <esphas@hotmail.com>
2017-02-23 13:37:28 +0800

    added :set, :get; enhanced LocalStorage

---
+++ lib/commands/set.rb
@@ -0,0 +1,20 @@
+module OpenRobot
+  module Command
+    def self.do_set str
+      _NO_KEY = "没有需要设置的值"
+      _KEY_ONLY_DOT = "不能以点号(.)为键值"
+      _DONE = "已记录"
+
+      lkey, value = str.split ?\s
+      return _NO_KEY if lkey.nil?
+
+      keys = lkey.split ?.
+      return _KEY_ONLY_DOT if keys.empty?
+
+      require 'lib/ls'
+      LocalStorage.set 'glob', keys, value
+
+      _DONE + ?\s + lkey + ?= + value
+    end
+  end
+end
