614b56346f251827dff4657d108895818b60fde4
Esphas Kueen <esphas@hotmail.com>
2017-02-24 19:38:06 +0800

    sa can now edit others' req

--- lib/commands/edit.rb
+++ lib/commands/edit.rb
@@ -1,17 +1,23 @@
 module OpenRobot
   module Command
     def self.do_edit(str)
+      _INVALID_FORMAT   = "编辑格式不对啦！"
+      _NO_PERMISSION    = "你不能改这个请求哦~"
+      _NO_REQUEST_FOUND = "哎呀，没有找到你要的那个请求！"
+
       if str =~ /\A(.*)\n([\w\W]*)\Z/
         id, content = $1, $2
         user_id = OpenRobot.current[3]
-        if !Request.execute("select id from request where user_id = ? and id = ?", user_id, id.to_i).empty?
-          Request.execute("delete from runtime_scripts where id = ?", id)
-          do_request(content)
-        else
-          return "Error 100: This request #{id} does not belong to you. #{user_id}"
-        end
+        oreq = Request.execute("select id, user_id from request where id = ?", id)
+        return _NO_REQUEST_FOUND if oreq.empty?
+        oreq = oreq[0]
+        return _NO_PERMISSION unless user_id == oreq[1] || Privilege.find_user_group(user_id).include?(1)
+        Request.execute 'delete from runtime_scripts where id = ?', id
+        OpenRobot.current[3] = oreq[1]
+        do_request content
+        OpenRobot.current[3] = user_id
       else
-        return "Error 99: Invalid Format"
+        _INVALID_FORMAT
       end
     end
   end
